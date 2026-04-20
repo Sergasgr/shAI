63719fbcb6c6aa5a4362528806fe48928036c5a7
Sergio Graciá <sergiogdc2005@gmail.com>
2026-04-20 18:47:19 +0200

    Fase 3: CLI interactivo, spinner y modo explicación

--- src/ai/ollama_client.py
+++ src/ai/ollama_client.py
@@ -1,4 +1,4 @@
-import httpx #hacerlo asíncrono
+import httpx
 
 def check_llm(url: str = 'http://localhost:11434/'):
     try:
@@ -9,17 +9,26 @@ def check_llm(url: str = 'http://localhost:11434/'):
     except httpx.RequestError:
         return False 
     
-def get_command(user_input: str) -> str:
-    instructions = " .You are a Linux Shell expert. Return ONLY the command to comply with the client's request without format neither explanations nor greetings"
-    prompt = user_input + instructions
+def get_command(user_input: str, context: dict) -> str:
+    instructions = f"You are a Linux Shell expert. Return ONLY the command to comply with the client's request without format neither explanations nor greetings. Take into account that the user is using the operative system {context['os']}, in the shell {context['shell']} and his preferred language {context['language']}"
     req = {
         "model": "qwen2.5-coder:1.5b",
-        "prompt": prompt,
+        "prompt": user_input,
+        "system": instructions,
         "stream": False
     }
     response = httpx.post("http://localhost:11434/api/generate", json=req)
     data = response.json()
-    return data["response"].replace("```bash", "").replace("```", "").strip()
+    return data["response"].replace("```bash", "").replace("```", "").replace("`", "").strip().split("\n")[0]
     
     
-    
+def get_explanation(command: str, context: dict) -> str:
+    instructions = f"You are a Linux Shell expert. Return a briefly explanation of the command {command} in the language {context['language']}. Be concise and direct."
+    req = {
+        "model": "qwen2.5-coder:1.5b",
+        "prompt": instructions,
+        "stream": False 
+    }
+    response = httpx.post("http://localhost:11434/api/generate", json=req)
+    data = response.json()
+    return data["response"].replace("`","'")
--- src/shai/cli.py
+++ src/shai/cli.py
@@ -1,14 +1,35 @@
 import typer 
-from ai.ollama_client import check_llm, get_command
+import subprocess
+from rich import print
+from rich.console import Console
+from typing import Annotated
+from ai.ollama_client import check_llm, get_command, get_explanation
+from shai.core.env_detector import get_system_context
 
 t = typer.Typer() 
+console = Console()
 
 @t.command()
-def principalCommand(prompt: str):
+def principalCommand(prompt: str, explanation: Annotated[bool, typer.Option(..., "--explain", "-e", help="Briefly explanation of the command in natural language")] = False):
     if not check_llm():
         typer.echo("LLM not on")
         raise typer.Exit(code=1)
-    print(get_command(prompt))
+    sys_context = get_system_context()
+    
+    with console.status("", spinner='dots'):
+        command = get_command(prompt, sys_context)
+    
+    print(f"[italic red]{command}") #typer.secho(f"{command}\n", fg=typer.colors.RED)
+    
+    if explanation:
+        with console.status("Generating explanation...", spinner='material'):
+            expl = get_explanation(command, sys_context)    
+        print(f"[italic cyan]{expl}")
+        
+    ex = typer.confirm("Do you want to execute it?")
+    if not ex:
+        raise typer.Exit(code=1)
+    subprocess.run(command, shell=True)
     
 if __name__ == "__main__":
     t()
